303d305cd78b001510a31c7227f580545f74c0e5
LadyCrispy <cvg.94@hotmail.com>
2021-01-17 20:08:02 +0100

    routes errors

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 var express = require('express');
 var createError = require('http-errors');
-var logger = require('morgan');
-
 var app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const routes = require('./routes/')(app);
 app.use('/static', express.static(__dirname + '/public/assets'));
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -24,11 +19,14 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.status(404).send({
+    "code": 404,
+    "message": "Route does not exist"
+  });
 });
 
 app.listen(3000, () => {
-  console.log(`Example app listening at http://localhost:3000`)
+  console.log(`App listening at http://localhost:3000`)
 })
 
 module.exports = app;
